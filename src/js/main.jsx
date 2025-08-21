c7ea517be5d67bd1b5c7eea88b4b60c2bd5d4540
Songbird98 <alancabcon@gmail.com>
2025-08-21 02:35:26 +0000

    first commit

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import PropTypes from 'prop-types';
 
 //Bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -9,10 +10,37 @@ import "bootstrap"
 import '../styles/index.css'
 
 // components
-import Home from './components/Home';
+function SimpleCounter(props){
+  return (<div className='bigCounter'>
+    <div className='calendar'>🕒</div>
+    <div className='four'>{props.digitFour % 10}</div>
+    <div className='three'>{props.digitThree % 10}</div>
+    <div className='two'>{props.digitTwo % 10}</div>
+    <div className='one'>{props.digitOne % 10}</div>
+
+  </div>);
+}
+ SimpleCounter.propTypes =  { 
+  digitFour: PropTypes.number,
+  digitThree: PropTypes.number,
+  digitTwo: PropTypes.number,
+  digitOne: PropTypes.number,
+ }
+
+
+
+let counter = 0;
+setInterval(function(){
+  const four = Math.floor(counter/1000);
+  const three = Math.floor(counter/100);
+  const two = Math.floor(counter/10);
+  const one = Math.floor(counter/1);
+  console.log(four,three,two,one);
+  counter++;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Home/>
+    <SimpleCounter digitOne={one} digitTwo={two} digitThree={three} digitFour={four}/>
   </React.StrictMode>,
-)
+);
+},1000);
